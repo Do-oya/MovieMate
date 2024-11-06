cfe6f042ddb4a72a3caeec753ccabdac9ad826ea
Do-oya <qkrwjdgus1441@naver.com>
2024-11-06 15:12:55 +0900

    chore: Java 21 및 Spring Boot 3.3.5로 업그레이드
    - 최신 언어 기능과 성능 개선을 위해 Java 버전을 21로 업데이트
    - Java 21과의 호환성을 위해 Spring Boot 버전을 3.3.5로 업그레이드
    - 새로운 Spring Boot 버전에 맞춰 종속성 조정

--- src/main/java/com/toy/moviemate/domain/review/mapper/ReviewMapper.java
+++ src/main/java/com/toy/moviemate/domain/review/mapper/ReviewMapper.java
@@ -5,9 +5,6 @@ import com.toy.moviemate.domain.review.entity.Review;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Component
 public class ReviewMapper {
 
