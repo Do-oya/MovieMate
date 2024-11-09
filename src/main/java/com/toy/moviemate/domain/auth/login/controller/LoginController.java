8a4e8c957ea3ccac829a9fa8dce0afe1312312d6
Do-oya <qkrwjdgus1441@naver.com>
2024-11-09 21:22:22 +0900

    feat: 유저페이지 관리자페이지 간단하게 구현
    - 권한 확인을 위한 페이지로 간단하게 구현

--- src/main/java/com/toy/moviemate/domain/auth/login/controller/LoginController.java
+++ src/main/java/com/toy/moviemate/domain/auth/login/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.toy.moviemate.domain.auth.login.controller;
 
+import com.toy.moviemate.domain.user.dto.UserDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
@@ -28,5 +30,13 @@ public class LoginController {
             new SecurityContextLogoutHandler().logout(request, response, authentication);
         }
         return "redirect:/login";
+    }
+
+    @GetMapping("/denied")
+    public String accessDenied(@RequestParam(value = "exception", required = false) String exception,
+                               @AuthenticationPrincipal UserDto userDto, Model model) {
+        model.addAttribute("username", userDto.getUsername());
+        model.addAttribute("exception", exception);
+        return "auth/denied";
     }
 }
