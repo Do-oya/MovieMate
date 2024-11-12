46379b94aaacc9fcff0a1119017c096fd9f31fa7
Do-oya <qkrwjdgus1441@naver.com>
2024-11-12 11:26:55 +0900

    feat: 매핑경로 변경

--- src/main/java/com/toy/moviemate/domain/admin/controller/AdminController.java
+++ src/main/java/com/toy/moviemate/domain/admin/controller/AdminController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
-    @GetMapping("/admin")
-    public String admin() {
-        return "admin/admin";
+    @GetMapping("/admin/home")
+    public String home() {
+        return "admin/home";
     }
 }
