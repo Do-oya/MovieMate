e55698dba9cac5edaa29b8a49074736d6fd70ef8
Do-oya <qkrwjdgus1441@naver.com>
2024-11-12 11:30:13 +0900

    feat: Spring Security UserDetailsService 구현 및 권한 로드 개선
    - `FormUserDetailsService` 클래스 추가:
      - `UserDetailsService` 인터페이스 구현
      - 사용자 인증 시 `loadUserByUsername()` 메서드를 통해 사용자 정보 및 권한을 로드
      - `UserRepository`를 사용하여 사용자 데이터를 조회
      - 조회된 사용자 엔티티(`User`)를 `UserDto`로 매핑하기 위해 `ModelMapper` 사용
      - 사용자 권한(`Role`)을 `GrantedAuthority`로 변환하여 반환
    
    - 권한 로드 개선:
      - `User` 엔티티에서 `UserRoles`(`Role` 리스트)를 조회하고, 권한 이름(`roleName`)을 `SimpleGrantedAuthority`로 변환
      - 스트림 API를 사용하여 권한 리스트 생성 및 가독성 향상
    
    - 예외 처리 추가:
      - 사용자 조회 실패 시 `UsernameNotFoundException` 예외를 발생시켜 Spring Security에서 처리 가능하게 함
    
    이 변경으로 사용자 인증 및 권한 로딩 과정이 효율적으로 개선되고, Spring Security와의 통합이 강화됨.

--- src/main/java/com/toy/moviemate/security/service/FormUserDetailsService.java
+++ src/main/java/com/toy/moviemate/security/service/FormUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.toy.moviemate.security.service;
 
+import com.toy.moviemate.domain.admin.entity.Role;
+import com.toy.moviemate.domain.admin.service.RoleService;
 import com.toy.moviemate.security.dto.UserContext;
 import com.toy.moviemate.domain.user.dto.UserDto;
 import com.toy.moviemate.domain.user.entity.User;
@@ -12,8 +14,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service("userDetailsService")
 @RequiredArgsConstructor
@@ -22,12 +26,17 @@ public class FormUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException("No user found with username: " + username);
         }
-        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(user.getRoles()));
+        List<GrantedAuthority> authorities = user.getUserRoles()
+                .stream()
+                .map(Role::getRoleName)
+                .collect(Collectors.toSet())
+                .stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
         ModelMapper mapper = new ModelMapper();
         UserDto userDto = mapper.map(user, UserDto.class);
         return new UserContext(userDto, authorities);
