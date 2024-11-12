13132f4f9f93838ab6c626bd8b30baa109e05016
Do-oya <qkrwjdgus1441@naver.com>
2024-11-12 11:31:33 +0900

    feat: 사용자 등록 서비스(UserService) 기능 추가 및 역할 연동 개선
    - `UserService` 클래스 구현:
      - 사용자 생성 메서드 `createUser(UserDto userDto)` 추가
      - 사용자 비밀번호 암호화를 위해 `PasswordEncoder` 사용
      - 사용자 엔티티(User) 빌더 패턴 적용
      - 사용자 저장 시 `UserRepository` 사용
    
    - 역할 연동 개선:
      - `RoleRepository`를 사용하여 기본 사용자 역할(`ROLE_USER`) 조회 및 설정
      - 사용자 생성 시 역할(`userRoles`)이 자동으로 할당되도록 수정

--- src/main/java/com/toy/moviemate/domain/user/service/UserService.java
+++ src/main/java/com/toy/moviemate/domain/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.toy.moviemate.domain.user.service;
 
+import com.toy.moviemate.domain.admin.entity.Role;
+import com.toy.moviemate.domain.admin.repository.RoleRepository;
 import com.toy.moviemate.domain.user.dto.UserDto;
 import com.toy.moviemate.domain.user.entity.User;
 import com.toy.moviemate.domain.user.repository.UserRepository;
@@ -10,20 +12,25 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Transactional
     public void createUser(UserDto userDto) {
+        Role role = roleRepository.findByRoleName("ROLE_USER");
         User user = User.builder()
                 .username(userDto.getUsername())
                 .password(passwordEncoder.encode(userDto.getPassword()))
                 .age(userDto.getAge())
-                .roles(userDto.getRoles())
+                .userRoles(new HashSet<>(Set.of(role)))
                 .build();
         userRepository.save(user);
     }
